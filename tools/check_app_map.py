01641e4dd8ba43cd5ce57ddd553846582332d71f
xcgspring <xcg1987xcg@126.com>
2014-09-04 16:56:17 +0800

    Add XML schema for AXUI functions
    Signed-off-by: xcgspring <xcg1987xcg@126.com>

---
+++ tools/check_app_map.py
@@ -0,0 +1,34 @@
+
+import os
+import sys
+try:
+    import pyxb
+except ImportError:
+    print("Need install pyxb first")
+    sys.exit(1)
+    
+from generate_all_schema_modules import generate_module
+
+def check_app_map(xs_module, app_map):
+    try:
+        app_map_instance = xs_module.CreateFromDocument(app_map)
+        print("Check successful")
+    except pyxb.UnrecognizedContentError as e:
+        print(e.details())
+        
+if __name__=="__main__":
+    print("Usage:\npython %s XSD_file(abs path) app_map_file" % __file__)
+    XSD_file = sys.argv[1]
+    app_map_file = sys.argv[2]
+    XSD_module_name = os.path.basename(XSD_file).split(".")[0]
+    XSD_module_dir = os.path.dirname(XSD_file)
+    generate_module(XSD_file, XSD_module_name, XSD_module_dir)
+    
+    sys.path.insert(0, XSD_module_dir)
+    XSD_module = __import__(XSD_module_name)
+    
+    with open(app_map_file) as app_map:
+        check_app_map(XSD_module, app_map.read())
+    
+    
+
---
+++ tools/generate_all_schema_modules.py
@@ -0,0 +1,105 @@
+
+import os
+import re
+import sys
+import platform
+try:
+    import pyxb
+except ImportError:
+    print("Need install pyxb first")
+    sys.exit(1)
+
+def generate_module(schema_location, module, module_root):
+    #pyxbgen script from /usr/local/bin/pyxbgen or c:\python27\scripts\pyxbgen
+    pyxbgen = '''
+#!/usr/bin/python
+
+import pyxb.xmlschema
+import pyxb.binding.generate
+import pyxb.utils.utility
+import pyxb.utils.domutils
+import os.path
+import sys
+import optparse
+
+import logging
+logging.basicConfig()
+log_ = logging.getLogger(__name__)
+
+def WSDLToSchema (generator, wsdl_uri):
+    """Extract the schema element from a WSDL types document and
+    generate bindings for it.
+
+    This is expected to be equivalent to retrieving the WSDL,
+    extracting the schema part of its C{types} block to a file, and
+    using that file as a C{--schema-location}.  Note that use of this
+    option may require availability of the WS-* bundle."""
+    import pyxb.bundles.wssplat.wsdl11 as wsdl
+    wsdl.ImportRelatedNamespaces()
+    wsdl_uri = pyxb.utils.utility.NormalizeLocation(wsdl_uri)
+    print 'Retrieving WSDL from %s' % (wsdl_uri,)
+    xmld = pyxb.utils.utility.DataFromURI(wsdl_uri)
+    spec = wsdl.definitions.createFromDOM(pyxb.utils.domutils.StringToDOM(xmld, location_base=wsdl_uri), process_schema=True, generation_uid=generator.generationUID())
+    return spec.schema()
+
+def _WSDLOptionCallback (option, opt_str, value, parser, *args, **kwargs):
+    assert parser.has_option('--schema-location')
+    if parser.values.schema_location is None:
+        parser.values.schema_location = []
+    parser.values.schema_location.append( (value, WSDLToSchema) )
+
+generator = pyxb.binding.generate.Generator()
+parser = generator.optionParser()
+group = optparse.OptionGroup(parser, 'WSDL Options', 'Options relevant to generating bindings from WSDL service descriptions')
+group.add_option('-W', '--wsdl-location', metavar='FILE_or_URL',
+                  action='callback', callback=_WSDLOptionCallback, type="string",
+                  help=\
+\'\'\'Generate bindings for the C{types} element of the WSDL at this
+location.\'\'\')
+parser.add_option_group(group)
+
+(options, args) = parser.parse_args(arguments)
+
+generator.applyOptionValues(options, args)
+
+generator.resolveExternalSchema()
+
+if 0 == len(generator.namespaces()):
+    parser.print_help()
+    sys.exit(1)
+
+import sys
+import traceback
+
+# Save binding source first, so name-in-binding is stored in the
+# parsed schema file
+try:
+    tns = generator.namespaces().pop()
+    modules = generator.bindingModules()
+    print 'Python for %s requires %d modules' % (tns, len(modules))
+
+    top_module = None
+    path_dirs = set()
+    for m in modules:
+        m.writeToModuleFile()
+
+    generator.writeNamespaceArchive()
+except Exception as e:
+    print 'Exception generating bindings: %s' % (e,)
+    traceback.print_exception(*sys.exc_info())
+    sys.exit(3)
+
+# LocalVariables:
+# mode:python
+# End:
+'''
+    exec(pyxbgen, {"arguments":["--schema-location=%s"%schema_location, "--module=%s"%module, "--binding-root=%s"%module_root]})
+    
+if __name__=="__main__":
+    current_dir=os.path.dirname(os.path.abspath(__file__))
+    xs_dir=os.path.join(current_dir, "..", "XML", "schemas")
+    for root, dirs, files in os.walk(xs_dir):
+        for name in files:
+            if re.match(".*\.xsd$", name) != None:
+                module_name = name.split(".")[0]
+                generate_module(os.path.join(root, name), module_name, root)
