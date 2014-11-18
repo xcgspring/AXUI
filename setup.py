4060fd9aeb1cc0584d2917e5f86619f2ec576200
xcgspring <xcg1987xcg@126.com>
2014-11-18 08:29:58 +0800

    Add setup script for python package
    Signed-off-by: xcgspring <xcg1987xcg@126.com>

---
+++ setup.py
@@ -0,0 +1,29 @@
+
+from distutils.core import setup
+
+setup(
+
+    name = "AXUI",
+    packages = ["AXUI", "AXUI.logger", "AXUI.parsing", "AXUI.XML"],
+    version = "0.1.0",
+    description = "UI auto framework",
+    author = "xcgspring",
+    author_email = "xcgspring@gmail.com",
+    url = "https://github.com/xcgspring/AXUI",
+    download_url = "",
+    keywords = ["UI", "auto"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing",
+        ],
+    long_description = ''' '''
+    
+)
