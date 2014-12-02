8b60fd3e26ca74f07bdb3aff8d807331cc0767e8
xcgspring <xcg1987xcg@126.com>
2014-12-02 09:45:14 +0800

    version 0.1.1
    Signed-off-by: xcgspring <xcg1987xcg@126.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name = "AXUI",
     packages = ["AXUI", "AXUI.logger", "AXUI.parsing", "AXUI.XML", "AXUI.driver", "AXUI.driver.windows", "AXUI.driver.windows.win32"],
     package_data = {"AXUI":["global.cfg"], "AXUI.XML":["schemas/AXUI_app_map.xsd"]},
-    version = "0.1.0",
+    version = "0.1.1",
     description = "UI auto framework",
     author = "xcgspring",
     author_email = "xcgspring@gmail.com",
