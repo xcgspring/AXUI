fee4a24efd5d9b34d75f84c2226c5381ccbbf185
xcgspring <xcg1987xcg@126.com>
2015-01-23 13:25:36 +0800

    Set DPI for win10
    Signed-off-by: xcgspring <xcg1987xcg@126.com>

--- AXUI/driver/windows/__init__.py
+++ AXUI/driver/windows/__init__.py
@@ -1,10 +1,12 @@
-#set DPI awareness for win8.1
+#set DPI awareness for win8.1 and win10
 from AXUI.logger import LOGGER
 import subprocess
 p = subprocess.Popen("wmic path win32_operatingsystem get version", stdout=subprocess.PIPE)
 (stdout, stderr) = p.communicate()
 windows_version = stdout.split()[1]
-if int(windows_version.split(".")[0]) >= 6 and int(windows_version.split(".")[1]) >= 3:
+#need set DPI awareness for win8.1 and win10
+if (int(windows_version.split(".")[0]) >= 6 and int(windows_version.split(".")[1]) >= 3) \
+    or int(windows_version.split(".")[0]) >= 10:
     LOGGER().debug("Set DPI awareness for windows verison: %s" % windows_version)
     import ctypes
     shcore = ctypes.windll.shcore
